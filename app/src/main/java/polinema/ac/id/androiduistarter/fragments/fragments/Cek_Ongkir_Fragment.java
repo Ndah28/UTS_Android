eca426625eea9e74ad1efcf6246e9f8ed8411749
Ndah28 <wahyuindah100@gmail.com>
2019-10-15 01:49:38 +0700

    Done

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/fragments/Cek_Ongkir_Fragment.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/fragments/Cek_Ongkir_Fragment.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,6 +21,11 @@ import polinema.ac.id.androiduistarter.R;
  */
 public class Cek_Ongkir_Fragment extends Fragment {
 
+    Button cek;
+    TextView berat;
+    RadioGroup rb1;
+    RadioButton r1, r2, r3, r4, r5, r6, r7, r8, r9;
+//    StringBuffer pilih_hobi;
 
     public Cek_Ongkir_Fragment() {
         // Required empty public constructor
@@ -26,7 +36,56 @@ public class Cek_Ongkir_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cek__ongkir, container, false);
-    }
+        final View rootView = inflater.inflate(R.layout.fragment_cek__ongkir, container, false);
+        cek  = (Button)rootView.findViewById(R.id.cek);
+        berat = (TextView) rootView.findViewById(R.id.berat);
+        rb1 = (RadioGroup)rootView.findViewById(R.id.rb1);
+        r1 = (RadioButton)rootView.findViewById(R.id.r1);
+        r2 = (RadioButton)rootView.findViewById(R.id.r2);
+        r3 = (RadioButton)rootView.findViewById(R.id.r3);
+        r4 = (RadioButton)rootView.findViewById(R.id.r4);
+        r5 = (RadioButton)rootView.findViewById(R.id.r5);
+        r6 = (RadioButton)rootView.findViewById(R.id.r6);
+        r7 = (RadioButton)rootView.findViewById(R.id.r7);
+        r8 = (RadioButton)rootView.findViewById(R.id.r8);
+        r9 = (RadioButton)rootView.findViewById(R.id.r9);
 
+        cek.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Integer kota = rb1.getCheckedRadioButtonId();
+                RadioButton rb1 = (RadioButton)rootView.findViewById(kota);
+
+                if (r1.isChecked()){
+                    Toast.makeText(getActivity(),"Kota asal-tujuan : "+rb1.getText() + "\n\n" + berat.getText() + "\n\n" + "Expedisi : " + "\n" + "Pos = Rp. 8000" +"\n" + "Jne = Rp. 8000"+"\n" + "Jnt = Rp. 10000", Toast.LENGTH_LONG).show();
+                }
+                if (r2.isChecked()){
+                    Toast.makeText(getActivity(),"Kota asal-tujuan : "+rb1.getText() + "\n\n" + berat.getText() + "\n\n" + "Expedisi : " + "\n" + "Pos = Rp. 8000" +"\n" + "Jne = Rp. 7000"+"\n" + "Jnt = Rp. 13000", Toast.LENGTH_LONG).show();
+                }
+                if (r3.isChecked()){
+                    Toast.makeText(getActivity(),"Kota asal-tujuan : "+rb1.getText() + "\n\n" + berat.getText() + "\n\n" + "Expedisi : " + "\n" + "Pos = Rp. 8000" +"\n" + "Jne = Rp. 8000"+"\n" + "Jnt = Rp. 14000", Toast.LENGTH_LONG).show();
+                }
+                if (r4.isChecked()){
+                    Toast.makeText(getActivity(),"Kota asal-tujuan : "+rb1.getText() + "\n\n" + berat.getText() + "\n\n" + "Expedisi : " + "\n" + "Pos = Rp. 8000" +"\n" + "Jne = Rp. 8000"+"\n" + "Jnt = Rp. 11000", Toast.LENGTH_LONG).show();
+                }
+                if (r5.isChecked()){
+                    Toast.makeText(getActivity(),"Kota asal-tujuan : "+rb1.getText() + "\n\n" + berat.getText() + "\n\n" + "Expedisi : " + "\n" + "Pos = Rp. 8000" +"\n" + "Jne = Rp. 8000"+"\n" + "Jnt = Rp. 12000", Toast.LENGTH_LONG).show();
+                }
+                if (r6.isChecked()){
+                    Toast.makeText(getActivity(),"Kota asal-tujuan : "+rb1.getText() + "\n\n" + berat.getText() + "\n\n" + "Expedisi : " + "\n" + "Pos = Rp. 8000" +"\n" + "Jne = Rp. 8000"+"\n" + "Jnt = Rp. 12000", Toast.LENGTH_LONG).show();
+                }
+                if (r7.isChecked()){
+                    Toast.makeText(getActivity(),"Kota asal-tujuan : "+rb1.getText() + "\n\n" + berat.getText() + "\n\n" + "Expedisi : " + "\n" + "Pos = Rp. 7000" +"\n" + "Jne = Rp. 7000"+"\n" + "Jnt = Rp. 9000", Toast.LENGTH_LONG).show();
+                }
+                if (r8.isChecked()){
+                    Toast.makeText(getActivity(),"Kota asal-tujuan : "+rb1.getText() + "\n\n" + berat.getText() + "\n\n" + "Expedisi : " + "\n" + "Pos = Rp. 8000" +"\n" + "Jne = Rp. 8000"+"\n" + "Jnt = Rp. 15000", Toast.LENGTH_LONG).show();
+                }
+                if (r9.isChecked()){
+                    Toast.makeText(getActivity(),"Kota asal-tujuan : "+rb1.getText() + "\n\n" + berat.getText() + "\n\n" + "Expedisi : " + "\n" + "Pos = Rp. 8000" +"\n" + "Jne = Rp. 8000"+"\n" + "Jnt = Rp. 14000", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        return rootView;
+    }
 }
--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/fragments/Cek_Resi_Fragment.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/fragments/Cek_Resi_Fragment.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,6 +21,10 @@ import polinema.ac.id.androiduistarter.R;
  */
 public class Cek_Resi_Fragment extends Fragment {
 
+    ImageButton btnCari;
+    EditText noResi;
+    RadioGroup ekspedisi;
+//    StringBuffer pilih_hobi;
 
     public Cek_Resi_Fragment() {
         // Required empty public constructor
@@ -26,7 +35,21 @@ public class Cek_Resi_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cek__resi, container, false);
-    }
+        final View rootView = inflater.inflate(R.layout.fragment_cek__resi, container, false);
+        btnCari = (ImageButton)rootView.findViewById(R.id.btnCari);
+        noResi = (EditText)rootView.findViewById(R.id.noResi);
+        ekspedisi = (RadioGroup)rootView.findViewById(R.id.ekspedisi);
 
+        btnCari.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Integer kirim = ekspedisi.getCheckedRadioButtonId();
+                RadioButton ekspedisi = (RadioButton)rootView.findViewById(kirim);
+
+                Toast.makeText(getActivity(),"Nomer Resi : "+noResi.getText() + "\n\n" + "Ekspedisi : "+ekspedisi.getText()+ "\n\n" + "Pengirim : hnbl31" + "\n\n" + "Penerima : Indah Wahyuningtyas" + "\n\n" + "Status : "+ "\n" + "08-08-2019 21:24[PASURUAN]Paket telah diterima"+ "\n" + "08-08-2019 11:34[PASURUAN]Paket akan dikirim ke alamat penerima"+ "\n" + "07-08-2019 02:01[SURABAYA]Paket telah sampai di SUB_GATEWAY"+ "\n" + "07-08-2019 09:35[BANDUNG]Paket akan dikirimkan ke SUB_GATEWAY", Toast.LENGTH_LONG).show();
+            }
+        });
+        return rootView;
+    }
 }
